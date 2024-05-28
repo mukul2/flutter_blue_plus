48a005515905b437a3cba6f89f87135b7d804355
MD AMINUL ISLAM MUKUL <saidur.shawon@gmail.com>
2024-05-28 11:07:04 +0100

    Update utils.dart

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -395,7 +395,7 @@ class _Mutex {
   int issued = 0;
 
   Future<bool> take() async {
-    await Future.delayed(Duration(milliseconds:200)); 
+    await Future.delayed(Duration(milliseconds:300)); 
     print("BLE SLOWED");
     int mine = issued;
     issued++;
